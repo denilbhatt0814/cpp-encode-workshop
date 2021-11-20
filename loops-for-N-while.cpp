f53f6f3906514bd0447301a10ad1ce9f86899126
Denil Bhatt <58869375+denilbhatt0814@users.noreply.github.com>
2021-11-20 12:45:54 +0530

    Create loops-for-N-while.cpp

---
+++ loops-for-N-while.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin>>n;
+
+  while(n!=0){
+    cout << n << endl;
+    n--;
+  }
+
+
+  // FOR LOOPS
+  // int n;
+  // cin>>n;
+
+  // for(int i = 0; i<n; i++){
+  //   int a;
+  //   cin >> a;
+  //   if(a%2 == 0){
+  //     cout<<"Is even"<<endl;
+  //   } else{
+  //     cout<<"Is odd"<<endl;
+  //   }
+  // }
+} 
