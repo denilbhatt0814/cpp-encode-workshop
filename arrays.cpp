b460056529261d28dc652f1f9d84b3572eb8dee6
Denil Bhatt <58869375+denilbhatt0814@users.noreply.github.com>
2021-11-20 12:43:31 +0530

    Create arrays.cpp

---
+++ arrays.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // Array -> collection of obj of similar type 
+  // indexing strats from 0
+
+  int arr[] = {10,20,30,50};
+  
+  // int arr[4]
+  int brr[4];
+  brr[0] = 1;
+  brr[1] = 2;
+  brr[2] = 3;
+  brr[3] = 4;
+
+  cout << arr[0] << endl;
+  cout << brr[2] << endl;
+}
