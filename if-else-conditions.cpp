f42aec215579a6a363e0be4e96f07e23a1313173
Denil Bhatt <58869375+denilbhatt0814@users.noreply.github.com>
2021-11-20 12:44:30 +0530

    Create if-else-conditions.cpp

---
+++ if-else-conditions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a;
+  cin >> a;
+
+  if(a%2 == 1){
+    cout<<"Is odd"<<endl;
+  }
+  else{
+    cout<<"Is even"<<endl;
+  }
+
+  return 0;
+}
