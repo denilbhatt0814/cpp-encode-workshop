1b98b3b87fee36937edeca8dba04712d94f9c8cd
Denil Bhatt <58869375+denilbhatt0814@users.noreply.github.com>
2021-11-20 12:40:28 +0530

    basics of math library

---
+++ mathsLib.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+// #include <cmath>
+
+using namespace std;
+
+int main() {
+  // pow(a,x) -> a^x
+  cout << pow(2,5) << endl;
+  
+  // for sqrt
+  cout << pow(2.0,0.5)<<endl;
+  //or (but pow is better than sqrt)
+  cout << sqrt(2) << endl;
+
+  // Ceil and floor
+  cout << ceil(1.5)<<endl;
+  cout<< floor(1.5) << endl;
+} 
