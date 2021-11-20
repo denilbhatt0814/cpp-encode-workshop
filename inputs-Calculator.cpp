74d2b830546b5421761b89d66a8b0d95859535e3
Denil Bhatt <58869375+denilbhatt0814@users.noreply.github.com>
2021-11-20 12:41:34 +0530

    Create inputs-Calculator.cpp

---
+++ inputs-Calculator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a,b;
+
+  cout<<"Enter 1st num: ";
+  cin >> a;
+
+  cout<<"Enter 2nd num: ";
+  cin >> b;
+
+  cout << a+b <<endl;
+  cout << a-b <<endl;
+  cout << a*b <<endl;
+  cout << a/b <<endl;
+  cout << a%b <<endl;
+
+  return 0;
+}
