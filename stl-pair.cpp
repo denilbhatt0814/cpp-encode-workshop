5e67818906df317b4953479aebae9ad14a6fd872
Denil Bhatt <58869375+denilbhatt0814@users.noreply.github.com>
2021-11-21 22:32:41 +0530

    Create stl-pair.cpp

---
+++ stl-pair.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+  Pair: collection of similar or unsimilar(datatypes) item
+*/
+
+int main() {
+  // Declaring & init a pair
+  pair<int, string> temp = {10, "Ten"};
+
+  // accessing elements of a pair
+  cout<< temp.first <<" -> "<< temp.second <<"\n";
+  
+  return 0;
+} 
